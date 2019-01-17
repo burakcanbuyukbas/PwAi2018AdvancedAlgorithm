42380c995c88163497a0a6c01fbfa5a179fd75e4
burakcanbuyukbas <burakcanbuyukbas@hotmail.com>
2019-01-17 14:45:46 +0100

    new matching algorithm

--- HousesAndWells/Form2.cs
+++ HousesAndWells/Form2.cs
@@ -16,7 +16,7 @@ namespace HousesAndWells
         List<Well> wells;
         List<House> houses;
         List<Well> kwells;
-        double totalDistance;
+        decimal totalDistance;
 
         public Form2(List<House> houseList, List<Well> wellList)
         {
@@ -40,7 +40,6 @@ namespace HousesAndWells
             dataGridView2.Columns["x"].Visible = false;
             dataGridView2.Columns["y"].Visible = false;
             dataGridView2.Columns["connectedWellName"].Visible = false;
-            //dataGridView2.Columns["distanceFromWell"].Visible = false;
             dataGridView1.Columns["connectedHouseString"].Visible = false;
 
             wells = wellList;
@@ -86,37 +85,42 @@ namespace HousesAndWells
                 house.Prefs = kwells.OrderBy(well => ((house.x - well.x) * (house.x - well.x) + (house.y - well.y) * (house.y - well.y))).ToList();
             }
 
+            ////Commented out to implement new matching algorithm
 
-            int freeHouseCount = houses.Count;
-            while (freeHouseCount > 0)
-            {
-                foreach (House house in houses)
-                {
-                    if (house.connectedWell == null)
-                    {
-                        Well well = house.NextCandidateNotYetProposedTo();
-                        if (well.connectedHouse == null)
-                        {
-                            house.EngageTo(well);
-                            freeHouseCount--;
-                        }
-                        else if (well.Prefers(house))
-                        {
-                            house.EngageTo(well);
-                        }
-                    }
-                }
-            }
+
+            //int freeHouseCount = houses.Count;
+            //while (freeHouseCount > 0)
+            //{
+            //    foreach (House house in houses)
+            //    {
+            //        if (house.connectedWell == null)
+            //        {
+            //            Well well = house.NextCandidateNotYetProposedTo();
+            //            if (well.connectedHouse == null)
+            //            {
+            //                house.EngageTo(well);
+            //                freeHouseCount--;
+            //            }
+            //            else if (well.Prefers(house))
+            //            {
+            //                house.EngageTo(well);
+            //            }
+            //        }
+            //    }
+            //}
+
+
+            RunMatching(houses, kwells); //algorithm v2
             var bindingListForMatchedHouses = new BindingList<House>(houses);
             dataGridView1.DataSource = bindingListForMatchedHouses;
             dataGridView1.Columns["x"].Visible = false;
             dataGridView1.Columns["y"].Visible = false;
             dataGridView1.Columns["connectedWell"].Visible = false;
             dataGridView1.Columns["connectedWellName"].Visible = true;
-            foreach (var house in houses)
+            foreach (var well in kwells)
             {
-                totalDistance += Math.Sqrt((house.x - house.connectedWell.x) * (house.x - house.connectedWell.x)
-                    + (house.y - house.connectedWell.y) * (house.y - house.connectedWell.y));
+                totalDistance += (decimal)(Math.Sqrt((well.x - well.connectedHouse.x) * (well.x - well.connectedHouse.x)
+                    + (well.y - well.connectedHouse.y) * (well.y - well.connectedHouse.y)));
             }
             labelTotalDistance.Visible = true;
             labelTotalDistance.Text = "Total Distance: " + totalDistance.ToString();
@@ -135,9 +139,9 @@ namespace HousesAndWells
 
                 for (int row = 0; row < dataGridView1.Rows.Count; row++)
                 {
-                    writer.WriteLine(dataGridView1.Rows[row].Cells[0].Value.ToString() + ". " 
-                        + dataGridView1.Rows[row].Cells[1].Value.ToString() + ": (" 
-                        +  dataGridView1.Rows[row].Cells[2].Value.ToString() + ","
+                    writer.WriteLine(dataGridView1.Rows[row].Cells[0].Value.ToString() + ". "
+                        + dataGridView1.Rows[row].Cells[1].Value.ToString() + ": ("
+                        + dataGridView1.Rows[row].Cells[2].Value.ToString() + ","
                         + dataGridView1.Rows[row].Cells[3].Value.ToString() + ") --> "
                         + dataGridView1.Rows[row].Cells[5].Value.ToString());
                 }
@@ -147,6 +151,64 @@ namespace HousesAndWells
 
             }
         }
+
+        //Matching Algorithm v2
+        private static void RunMatching(List<House> wells, List<Well> houses)
+        {
+            foreach (var well in wells)
+            {
+                var matchings = new List<Well>();
+                while (well.connectedWell == null)
+                {
+                    var foundMatch = false;
+                    Well match = null;
+                    while (!foundMatch)
+                    {
+                        match = houses.Find(x => x == well.Prefs.First());
+                        if (!matchings.Contains(match))
+                            foundMatch = true;
+                        else
+                            // --Move the matched house to the back of well's preferences in order to grab the next one which might not be matched
+                            MoveFirstElementToEnd(well.Prefs);
+                    }
+
+                    if (match.connectedHouse == null)
+                        MarkAsMatched(well, match);
+
+                    else
+                    {
+                        // --Compare preference of current match and new match, cancelling the current match out if the potential match is a higher preference
+                        var currentMatchIndex = match.Prefs.IndexOf(match.connectedHouse);
+                        var potentialMatchIndex = match.Prefs.IndexOf(well);
+
+                        if (potentialMatchIndex < currentMatchIndex)
+                        {
+                            var currentMatch = wells.Find(m => m == match.connectedHouse);
+                            currentMatch.connectedWell = null;
+                            MarkAsMatched(well, match);
+                        }
+                        else
+                            matchings.Add(match);
+                    }
+                }
+            }
+            // --Recursively run through the program if not all of the proposing group is matched (i.e. if someone had their match taken away from them)
+            if (wells.Any(m => m.connectedWell == null))
+                RunMatching(wells, houses);
+        }
+        private static void MoveFirstElementToEnd<T>(List<T> list) where T : class
+        {
+            var temp = list[0];
+            list.Remove(temp);
+            list.Add(temp);
+        }
+        private static void MarkAsMatched(House house, Well well)
+        {
+            house.connectedWell = well;
+            well.connectedHouse = house;
+        }
+
     }
+
 }
 
